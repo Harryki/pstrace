5f94a5b7b224c6546acc94546c0330a00cdf2b0f
Yeonghun Ki <yeonghun.ki@gmail.com>
2025-03-30 22:36:12 -0700

    height updated

--- model.go
+++ model.go
@@ -31,10 +31,10 @@ func NewCallGraphTUIModel(paths [][]string, target string) Model {
 		}
 	}
 
-	height := len(callers) + 2 // +2 for title + padding
-	listModel := list.New(callers, list.NewDefaultDelegate(), 20, height)
+	// height := len(callers) + 2 // +2 for title + padding
+	listModel := list.New(callers, list.NewDefaultDelegate(), 25, 50)
 	listModel.Title = "Top-Level Callers"
-	vp := viewport.New(60, 20)
+	vp := viewport.New(60, 40)
 	vp.SetContent(renderPaths(mockPaths[callers[0].FilterValue()], target))
 
 	return Model{
